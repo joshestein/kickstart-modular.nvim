c2f50289c158b23f0704f063e3d5543d086cac88
Josh <josh.e.stein@gmail.com>
2024-03-26 11:04:42 -0400

    Update quickscope

--- lua/custom/plugins/quickscope.lua
+++ lua/custom/plugins/quickscope.lua
@@ -1,25 +1,14 @@
 return {
   'unblevable/quick-scope',
-  config = function()
-    vim.cmd [[
-      let g:qs_highlight_on_keys = ['f', 'F', 't', 'T']
-      let g:qs_max_chars=120
-      augroup qs_colors
-        autocmd!
-        autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
-      augroup END
-    ]]
-    -- local qs_colors = vim.api.nvim_create_augroup('qs_colors', { clear = true })
-    -- vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
-    --   pattern = '*',
-    --   group = qs_colors,
-    --   command = 'highlight QuickScopePrimary guifg="#afff5f" gui=underline ctermfg=155 cterm=underline',
-    -- })
-    -- vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
-    --   pattern = '*',
-    --   group = 'qs_colors',
-    --   command = 'highlight QuickScopeSecondary guifg="#5fffff" gui=underline ctermfg=81 cterm=underline',
-    -- })
+  init = function()
+    vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+    vim.g.qs_max_chars = 120
+    -- vim.cmd [[
+    --   augroup qs_colors
+    --     autocmd!
+    --     autocmd ColorScheme * highlight QuickScopeSecondary guifg=#5fffff gui=underline ctermfg=81 cterm=underline
+    --     autocmd ColorScheme * highlight QuickScopePrimary guifg=#afff5f gui=underline ctermfg=155 cterm=underline
+    --   augroup end
+    -- ]]
   end,
-  enabled = false,
 }
